c09378c05a3e4285ed751f9c266b430c823a096b
rodrigoaives <rodrigoaives@outlook.com>
2017-05-18 20:07:30 -0300

    Jogo da Velha em C#.

--- JogoDaVelha/Jogo.Designer.cs
+++ JogoDaVelha/Jogo.Designer.cs
@@ -54,7 +54,7 @@
             this.lblVez.BackColor = System.Drawing.Color.Transparent;
             this.lblVez.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblVez.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblVez.Location = new System.Drawing.Point(170, 23);
+            this.lblVez.Location = new System.Drawing.Point(170, 28);
             this.lblVez.Name = "lblVez";
             this.lblVez.Size = new System.Drawing.Size(318, 41);
             this.lblVez.TabIndex = 0;
